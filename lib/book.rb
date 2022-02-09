dff29038b3769253a28c563e659909efb592e697
Doris Morales <moralesdoris880@gmail.com>
2022-02-09 13:41:50 -0500

    commit tree 362bcb2f4d69896919dcbf2bec9f846dc830689d

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,25 @@
+class Book
+    attr_accessor :title
+    attr_accessor :author
+    attr_accessor :page_count
+    attr_accessor :genre
+    def initialize(author)
+        @author =author
+    end
+    def initialize(page_count)
+        @page_count=page_count
+    end
+    def initialize(genre)     
+        @genre = genre
+    end
+    def turn_page
+       puts "Flipping the page...wow, you read fast!"
+    end
+    def initialize title
+        @title=title
+    end
+
+
+end
 
 
