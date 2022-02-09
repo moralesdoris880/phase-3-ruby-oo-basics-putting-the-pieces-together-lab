4e46e829c0021edfa15b9ea1c928aa0e7cc1b129
Doris Morales <moralesdoris880@gmail.com>
2022-02-09 13:42:03 -0500

    Completed assignment

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,30 @@
-# Make your shoe class here!
+class Shoe
+    attr_accessor :brand
+    attr_accessor :color
+    attr_accessor :size
+    attr_accessor :material
+    attr_accessor :condition
+    def initialize color
+        @color= color
+    end
+    def initialize size
+        @size= size
+    end
+    def initialize material
+        @material= material
+    end
+    def initialize condition
+        @condition= condition
+    end
+    def cobble
+        if @condition=="old"
+            @condition="new"
+        end
+        puts "Your shoe is as good as new!"     
+    end    
+    def initialize brand
+        @brand= brand
+    end
+    
+
+end
